ab93fdd8ca0852645fcdc1ac20ff68571507fedf
jinmi2025 <greenu19@naver.com>
2025-03-21 17:34:18 +0900

    0321 헤더까지

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./header/Header";
+import MainTop from "./banner/MainTop";
+import Side from "./header/Side";
+
+export { Header, MainTop, Side };
---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+
+    return (
+        <div id="main-container">
+            메인페이지 본문 영역
+        </div>
+    )
+}
+
+export default Main;
